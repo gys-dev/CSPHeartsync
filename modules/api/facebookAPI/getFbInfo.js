139b0ddcde1aa4365a18166638d4e7059e7f88a3
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2017-12-16 09:25:08 +0700

    Added getFbInfo

---
+++ modules/api/facebookAPI/getFbInfo.js
@@ -0,0 +1,26 @@
+var request = require('request');
+var getFbInfo = (senderId) => {
+    return new Promise((resolve, reject) => {
+        request({
+            url: 'https://graph.facebook.com/v2.11/' + senderId,
+            qs: {
+                access_token: "EAALBIgDENZB8BAJYSxchUNyB0wECTzNo5ZCSEzt2WlZA0fbTeH4GsiZAZCmSVYToeRdGJD0l7ZBUZB9ZBKdlRdqN0ptmiVVCXFix8GrT5jaOq0J3jtkNZC7AtZBZCToLwh8ajrzsMChgI12lugnamE1CQBm9crBqdpBxXYDdy4iqZCZCSoORMgZCRwr53R"
+            },
+            method: "GET"
+        }, (err, res, body) => {
+            if (err) return reject(err);
+            body = JSON.parse(body);
+            var obj = { name: body.last_name + " " + body.first_name, 
+            profile_pic: body.profile_pic, 
+            gender: body.gender, 
+            _id: body.id, 
+            preferences:{
+                favorite: 0, //0 random, 1 male, 2 female
+                inconversation: 0 //0 no, 1 pending, 2 yep
+            }};
+            resolve(obj);
+        })
+    })
+}
+
+module.exports = { getFbInfo: getFbInfo };
