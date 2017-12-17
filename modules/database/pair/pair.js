e9f62c9c323123c93247c3ae9030f5225101a8b5
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2017-12-17 17:20:32 +0700

    Fix pair.js

--- modules/database/pair/pair.js
+++ modules/database/pair/pair.js
@@ -1,35 +1,78 @@
-var MongoClient = require('mongodb').MongoClient,
+var mongodb = require('mongodb').MongoClient,
     url = 'mongodb://localhost:27017';
 
 var pair = (senderId, gender, fav) => {
     if (fav != 'none') {
         if (fav === gender) {
-            MongoClient.connect(url, (err, db) => {
-                let dbase = db.db('cspheartsync');let collect = dbase.collection('pending');
-                collect.find({ gender: gender, favorite: fav }).limit(1).toArray(function (err, res) {
-                    var partnerId = res[0]._id;
-                    if (partnerId != '') {
-                        collect.deleteOne({ _id: senderId.toString() });
+            mongodb.connect(url, (err, db) => {
+                let dbase = db.db('cspheartsync');
+                let collect = dbase.collection('pending');
+                collect.count({ gender: gender, favorite: fav }).then(count => {
+                    if (count < 2) {
+                        return console.log("Oh noo =))) Không có user nào");
+                    }
+                    collect.deleteOne({ _id: senderId.toString() });
+                    collect.find({ gender: gender, favorite: fav }).limit(1).toArray((err, result) => {
+                        let partnerId = result[0]._id;
                         collect.deleteOne({ _id: partnerId.toString() });
                         let paired = dbase.collection('paired');
-                        paired.insert({ id1: senderId.toString(), id2: partnerId.toString() }, (err, res) => {
-                    //         //Trả về có bạn
-                        });
-                     }
+                        paired.insert({ id1: senderId.toString(), id2: partnerId.toString() });
+                        paired.insert({ id1: partnerId.toString(), id2: senderId.toString() })
+                    })
+                })
+            })
+        }
+        
+        if (fav != gender) {
+            mongodb.connect(url, (err, db) => {
+                let dbase = db.db('cspheartsync');
+                let collect = dbase.collection('pending');
+                collect.deleteOne({ _id: senderId.toString() });
+
+                collect.count({ gender: fav, favorite: gender }).then(count => {
+                    if (count < 1) {
+                        return console.log("Oh noo =))) Không có user nào");
+                    }
+                    collect.find({ gender: fav, favorite: gender }).limit(1).toArray((err, result) => {
+                        let partnerId = result[0]._id;
+                        collect.deleteOne({ _id: partnerId.toString() });
+                        let paired = dbase.collection('paired');
+                        paired.insert({ id1: senderId.toString(), id2: partnerId.toString() });
+                        paired.insert({ id1: partnerId.toString(), id2: senderId.toString() })
+                    })
+                })
+            })
+        }
+    }
+        if (fav === 'none') {               
+            console.log(fav)    
+            
+            mongodb.connect(url, (err, db) => {
+                let dbase = db.db('cspheartsync');
+                let collect = dbase.collection('pending');
+                collect.count({ favorite: "none" }).then(res => {
+                    if (res >= 2) {
+                        collect.deleteOne({ _id: senderId.toString() });
+                        collect.find({ favorite: "none" }).limit(1).toArray((err, result) => {
+                            let partnerId = result[0]._id;
+                            collect.deleteOne({ _id: partnerId.toString() });
+                            let paired = dbase.collection('paired');
+                            paired.insert({ id1: senderId.toString(), id2: partnerId.toString() });
+                            paired.insert({ id1: partnerId.toString(), id2: senderId.toString() })
+                        })
+                    }
                     else {
-                        //Trả về không có trai :<
+                        collect.deleteOne({ _id: senderId.toString() });
+                        collect.find({ favorite: gender }).limit(1).toArray((err, result) => {
+                            let partnerId = result[0]._id;
+                            collect.deleteOne({ _id: partnerId.toString() });
+                            let paired = dbase.collection('paired');
+                            paired.insert({ id1: senderId.toString(), id2: partnerId.toString() });
+                            paired.insert({ id1: partnerId.toString(), id2: senderId.toString() })
+                        })
                     }
                 })
             })
         }
-        if (fav != gender) {
-
-        }
-    }
-    else {
-
-    }
 }
-
-
-pair(123123, "male", "male")
+pair("1212","female",'none');
---
+++ modules/database/pair/pending.js
@@ -0,0 +1,18 @@
+var mongodb = require('mongodb').MongoClient,
+    url = 'mongodb://localhost:27017',
+    getFav = require('../checkUser/checkFav'),
+    getGender = require('../checkUser/checkGender'),
+    pair = require('./pair');
+var pending = (senderId) => {
+    let favorite = await getFav.checkFav(senderId);
+    let gender = await getGender.checkGender(senderId);
+    mongodb.connect(url, (err, db) => {
+        let collect = db.db('cspheartsync').collection('pending');
+        collect.insert({ _id: senderId.toString(), favorite: favorite, gender: gender });
+        collect.count.then(res => {
+            if (res >= 2) {
+                pair(senderId.toString(), gender,fav);
+            }
+        })
+    })
+}
