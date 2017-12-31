0fb0e91ebb6f33bd29c36a636982aef5fc77efda
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2017-12-31 17:13:44 +0700

    added change inconver

--- modules/database/pair/pending.js
+++ modules/database/pair/pending.js
@@ -2,7 +2,8 @@ var mongodb = require('mongodb').MongoClient,
     url = 'mongodb://localhost:27017',
     getFav = require('../checkUser/checkFav'),
     getGender = require('../checkUser/checkGender'),
-    pair = require('./pair');
+    pair = require('./pair'),
+    inconverPending = require('../resUser/inconverPending');
 var pending = async (senderId) => {
     let favorite = await (getFav.checkFav(senderId));
     let gender = await (getGender.checkGender(senderId));
@@ -15,15 +16,17 @@ var pending = async (senderId) => {
             gender: gender
         }, (err, res) => {
             if (err) throw err;
-            collect.count().then(res => {
-                if (res >= 2) {
-                    collect.find().skip(res - 1).limit(1).toArray((err, result) => {
-                        // pair(res[0]._id.toString(), res[0]._id.gender, res[0]._id.fav);
-                        // console.log(result);
-                       pair.pair(result[0]._id.toString(), result[0].gender, result[0].favorite);
+            inconverPending.inconverPending(senderId)
+                .then(res => {
+                    collect.count().then(res => {
+                        if (res >= 2) {
+                            collect.find().skip(res - 1).limit(1).toArray((err, result) => {
+                                pair.pair(result[0]._id.toString(), result[0].gender, result[0].favorite);
+                            })
+                        }
                     })
-                }
-            })
+                })
+
         });
 
     })
---
+++ modules/database/resUser/inconverPending.js
@@ -0,0 +1,11 @@
+var mongodb = require('mongodb').MongoClient,
+url="mongodb://localhost:27017";
+
+var inconverPending = (senderId)=>{
+    return new Promise((resolve,reject)=>{
+        mongodb.connect(url,(err,db)=>{
+            if(err) throw err;
+            db.db('cspheartsync').collection('users').update({_id:senderId.toString()},{$set:{inconversation: 1}},(err,res)=>{resolve('ok')})
+        })
+    })
+}
