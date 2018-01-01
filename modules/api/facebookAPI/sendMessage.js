6f8a75e07a175b3aa7c3c8461536ac364153d646
vietan <vietanisme@gmail.com>
2018-01-01 19:25:00 +0700

    finishing touch 2

--- modules/api/facebookAPI/sendMessage.js
+++ modules/api/facebookAPI/sendMessage.js
@@ -19,12 +19,10 @@ class sendMessage {
                         "type": "template",
                         "payload": {
                             "template_type": "generic",
-                            "elements": [
-                                {
-                                    "title": title,
-                                    "subtitle": content
-                                }
-                            ]
+                            "elements": [{
+                                "title": title,
+                                "subtitle": content
+                            }]
                         }
                     }
                 }
@@ -51,16 +49,15 @@ class sendMessage {
                             "type": "template",
                             "payload": {
                                 "template_type": "generic",
-                                "elements": [
-                                    {
-                                        "title": title,
-                                        "subtitle": content
-                                    }
-                                ]
+                                "elements": [{
+                                    "title": title,
+                                    "subtitle": content
+                                }]
                             }
                         }
                     }
-                }, (err, res, body) => {
+                },
+            }, (err, res, body) => {
                 if (err) throw (err);
                 if (res.body.error) throw (res.body.error);
                 resolve('ok')
@@ -89,25 +86,25 @@ class sendMessage {
     }
     sendImage(senderId, url) {
         request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {
-                access_token: this._token
-            },
-            method: 'POST',
-            json: {
-                recipient: {
-                    id: senderId
+                url: 'https://graph.facebook.com/v2.6/me/messages',
+                qs: {
+                    access_token: this._token
                 },
-                message: {
-                    attachment: {
-                        type: "image",
-                        payload: {
-                            url: url
-                        }
+                method: 'POST',
+                json: {
+                    recipient: {
+                        id: senderId
                     },
+                    message: {
+                        attachment: {
+                            type: "image",
+                            payload: {
+                                url: url
+                            }
+                        },
+                    }
                 }
-            }
-        },
+            },
             (err, res, body) => {
                 if (err) return console.log("Error: " + err)
                 if (res.body.error) return console.log("err: " + res.body.error)
@@ -130,8 +127,7 @@ class sendMessage {
                         payload: {
                             template_type: "button",
                             text: "Bạn muốn người ghép đôi tiếp theo có giới tính gì nào?",
-                            buttons: [
-                                {
+                            buttons: [{
                                     type: "postback",
                                     payload: "SELECT_MALE",
                                     title: "Nam nè :D"
@@ -151,11 +147,10 @@ class sendMessage {
                     }
                 }
             }
-        }
-            , (err, res, body) => {
-                if (err) return console.log("Error: " + err)
-                if (res.body.error) return console.log("err: " + res.body.error)
-            })
+        }, (err, res, body) => {
+            if (err) return console.log("Error: " + err)
+            if (res.body.error) return console.log("err: " + res.body.error)
+        })
     }
     sendTextMessageWithPromise(senderId, text) {
         return new Promise((resolve, reject) => {
@@ -182,25 +177,25 @@ class sendMessage {
     }
     sendVideo(senderId, url) {
         request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {
-                access_token: this._token
-            },
-            method: 'POST',
-            json: {
-                recipient: {
-                    id: senderId
+                url: 'https://graph.facebook.com/v2.6/me/messages',
+                qs: {
+                    access_token: this._token
                 },
-                message: {
-                    attachment: {
-                        type: "video",
-                        payload: {
-                            url: url
-                        }
+                method: 'POST',
+                json: {
+                    recipient: {
+                        id: senderId
                     },
+                    message: {
+                        attachment: {
+                            type: "video",
+                            payload: {
+                                url: url
+                            }
+                        },
+                    }
                 }
-            }
-        },
+            },
             (err, res, body) => {
                 if (err) return console.log("Error: " + err)
                 if (res.body.error) return console.log("err: " + res.body.error)
@@ -208,25 +203,25 @@ class sendMessage {
     }
     sendAudio(senderId, url) {
         request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {
-                access_token: this._token
-            },
-            method: 'POST',
-            json: {
-                recipient: {
-                    id: senderId
+                url: 'https://graph.facebook.com/v2.6/me/messages',
+                qs: {
+                    access_token: this._token
                 },
-                message: {
-                    attachment: {
-                        type: "audio",
-                        payload: {
-                            url: url
-                        }
+                method: 'POST',
+                json: {
+                    recipient: {
+                        id: senderId
                     },
+                    message: {
+                        attachment: {
+                            type: "audio",
+                            payload: {
+                                url: url
+                            }
+                        },
+                    }
                 }
-            }
-        },
+            },
             (err, res, body) => {
 
                 console.log(res.body.error)
